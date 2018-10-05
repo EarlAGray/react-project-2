be83de9e10338d46af5a769e7bd073a83e67b146
E.A. Gray <earl.a.gray@accenturefederal.com>
2018-10-05 10:08:30 -0400

    Chapter 4 Changes
    Chapter 4 changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+// in JSX, you always need to use import from images unlike CSS, but use this for single images, use technique below for multiple
+// would be  <Link to="/"><img src={logo} width="30" /></Link> and use below 
+// import logo from '/Users/earl.a.gray/Documents/GitHub/react-project/public/images/logo-designcode.svg'
+import './header.css'
+
 
 const Header = ({ siteTitle }) => (
   <div className="Header"> 
     <div className="HeaderGroup">
-    Hello
+      <Link to="/"><img src={require('/Users/earl.a.gray/Documents/GitHub/react-project/public/images/logo-designcode.svg')} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
