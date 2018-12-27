2eafd926263ecbe1e1a8b4f6a294681a139dfaa2
E.A. Gray <earl.a.gray@accenturefederal.com>
2018-12-27 14:41:47 -0500

    2017 is ENDING Again!
    Woooh!

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <h1>E.A. Gray <br /> Currently under construction</h1>
-        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
+        <p>Thanks to DesignCode.io, I am currently building this website using ReactJS.</p>
         <Link to="/page-2/">Watch the video</Link>
         <div className="Logos">
           <img src={require('../../public/images/logo-sketch.png')} width="50"  alt=""/> 
